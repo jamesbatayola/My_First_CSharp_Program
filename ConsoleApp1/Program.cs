7e55b2d98036d8bfbde4404ec4ed702084a27c83
jamesbatayola <batayolajakejames>
2024-05-27 17:39:48 +0800

    for loop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-int num = 10;
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            
+            int num = 10;
 
-for (int i = 0; i < num; i++) {
-    Console.WriteLine("Hello World");
+            for (int i = 0; i < num; i++) {
+                Console.WriteLine("Hello World");
+            }
+            
+        }
+        
+    }
+    
 }
+
+
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d008650f5bb41c88939f8c8d4ee160fad4325a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
